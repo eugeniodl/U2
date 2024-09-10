6de00dd6c920f26099fdaa5acda385a4ec775b5f
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-09-10 08:38:25 -0600

    ClasesMetodosEstaticos, ClasesParciales, ExtensionMetodos, TipoENUM

---
+++ ExtensionMetodos/Program.cs
@@ -0,0 +1,33 @@
+﻿string message = "Hello World";
+string upperMessage = message.ToUpperIfContainsLower();
+Console.WriteLine(upperMessage);
+
+var people = new Person[]
+{
+    new Person { Name = "Leticia", Age = 30 },
+    new Person { Name = "Rodrigo", Age = 18 },
+    new Person { Name = "Ernesto", Age = 17 },
+    new Person { Name = "Tania", Age = 24 },
+    new Person { Name = "Pedro", Age = 21 }
+};
+var adults = people.FilterByAge(21);
+
+foreach (Person person in adults)
+    Console.WriteLine($"{person.Name}, Age: {person.Age}");
+
+
+public static class StringExtensions
+{
+    public static string ToUpperIfContainsLower(this string str)
+    {
+        if(string.IsNullOrEmpty(str))
+            return str;
+
+        if(str.Any(char.IsLower))
+        {
+            return str.ToUpper();
+        }
+
+        return str;
+    }
+}
---
+++ TipoENUM/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+public enum OrderStatus
+{
+    Pending,
+    Processing,
+    Shipped,
+    Delivered,
+    Canceled
+}
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public OrderStatus Status { get; set; }
+
+    public void UpdateStatus(OrderStatus newStatus)
+    {
+        // Ejemplo de lógica para actualizar el estado
+        if(Status == OrderStatus.Canceled || Status == OrderStatus.Delivered)
+        {
+            throw new InvalidOperationException("Cannot change status from current state.");
+        }
+
+        Status = newStatus;
+    }
+
+    public override string ToString()
+    {
+        return $"Order {OrderId}: {Status}";
+    }
+}
